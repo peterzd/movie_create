d1c5ff4b483c4e1148a618bf227ea88a6e4db704
peterzd <peterzd@gmail.com>
2012-12-20 22:34:04 +0800

    init project

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+
+  gem 'therubyracer'
+
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+	gem 'database_cleaner'
+	gem 'webrat'
+end
+
+gem 'cucumber-rails', group: [:test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieCreate::Application.config.secret_token = 'a275093de0809747b436c97cb7bde39388124329d17b50c5935f29f85c40e847e7cb994e17f112f1a1f1c6be3568d506a58c447ebe570756848b1c0e12ba5377'
